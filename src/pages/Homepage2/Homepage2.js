9e112ab0ef5c1b229e733444d86aad1b47039501
kincaid1207 <73080418+kincaid1207@users.noreply.github.com>
2023-06-10 21:51:50 -0400

    updating Homepage2

--- src/pages/Homepage2/Homepage2.js
+++ src/pages/Homepage2/Homepage2.js
@@ -2,44 +2,47 @@ import React from "react";
 import Miles from "../../images/Miles_Morales.png";
 import Tony from "../../images/Tony_Stark.png";
 import Kang from "../../images/Kang.png";
-import "./Hero.css";
-import "../../fonts/Poppins-Regular.ttf";
-import "../../fonts/RobotoFlex-Regular.ttf";
+import "./Hoempage2.css";
 function Homepage2() {
   return (
-    <>
-      <section>
-        <div className="hero__left">
-          <span className="hero__left__header">
-
-          </span>
-      </section>
-      <section>
-        <div className="hero__right">
-          <img
-            className="hero__featured-poster1"
-            src={Miles}
-            alt="Miles Morales"
-          />
-          <img className="hero__featured-poster2" src={Tony} alt="Iron Man" />
-          <img className="hero__featured-poster3" src={Kang} alt="Kang" />
-          <div className="hero__featured-rectangle1"></div>
-          <div className="hero__featured-rectangle2"></div>
+    <div className="homepage">
+      <section className="hero">
+        <div className="container">
+          <div className="hero__content">
+            <div className="hero__left">
+              <h1>Featured AI-Generated Posters</h1>
+              <p>
+                Dive into the amazing Marvel Cinematic Unviers with art work
+                generated throughout the mutliverse
+              </p>
+              <a href="">Browse our favorites</a>
+            </div>
+            <div className="hero__right">
+              <img src="" alt="" />
+              <img src="" alt="" />
+              <img src="" alt="" />
+              <div></div>
+              <div></div>
+            </div>
+            <div className="hero__background hero__circle1"></div>
+            <div className="hero__background hero__circle2"></div>
+            <div className="hero__background hero__circle3"></div>
+            <div className="hero__background hero__circle4"></div>
+            <div className="hero__background hero__circle5"></div>
+            <div className="hero__background hero__circle6"></div>
+            <div className="hero__background hero__circle7"></div>
+          </div>
         </div>
       </section>
 
-      <section>
-        <div className="hero__background">
-          <div id="circle1"></div>
-          <div id="circle2"></div>
-          <div id="circle3"></div>
-          <div id="circle4"></div>
-          <div id="circle5"></div>
-          <div id="circle6"></div>
-          <div id="circle7"></div>
-        </div>
+      <section className="popular">
+        <div className="container"></div>
       </section>
-    </>
+
+      <section className="promotion">
+        <div className="container"></div>
+      </section>
+    </div>
   );
 }
 
